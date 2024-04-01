e8425cff57495e0e2ac92da98b2057e092035c22
hanwool95 <hanwool95@snu.ac.kr>
2024-04-01 23:21:50 +0900

    REFACTOR: make tokenizer class

--- tokenizer.py
+++ tokenizer.py
@@ -1,18 +1,36 @@
-import torch
-from transformers import AutoTokenizer
 import pandas as pd
+from transformers import AutoTokenizer
 
 
-def tokenize_function(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
+class DataTokenizer:
+    def __init__(self, model_name: str):
+        """
+        Constructor for the DataTokenizer class.
+        :param model_name: Name of the model to be used.
+        """
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def tokenize_function(self, examples):
+        """
+        Function to tokenize the given examples.
+        :param examples: A list of texts to be tokenized.
+        :return: Tokenized output.
+        """
+        return self.tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    def get_tokenized_data(self, data_url: str):
+        """
+        Loads data from a CSV file and tokenizes it.
+        :param data_url: Path to the data file.
+        :return: Tokenized data.
+        """
+        df = pd.read_csv(data_url)
+        df['text'] = df['Messages'].fillna('') + " " + df['Remain Messages'].fillna('')
+        prepared_data = {"text": df['text'].tolist()}
+        return self.tokenize_function(prepared_data)
 
 
-torch.cuda.empty_cache()
-tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
-
-df = pd.read_csv('data.csv')
-df['text'] = df['Messages'].fillna('') + " " + df['Remain Messages'].fillna('')
-
-prepared_data = {"text": df['text'].tolist()}
-
-tokenized_data = tokenize_function(prepared_data)
+if __name__ == '__main__':
+    tokenizer = DataTokenizer("bert-base-multilingual-cased")
+    tokenized_data = tokenizer.get_tokenized_data('data.csv')
+    print(tokenized_data['input_ids'][0])
