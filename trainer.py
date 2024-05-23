48f67deaa1b5c5208f1aea5962805cb994f41ac1
hanwool95 <hanwool95@snu.ac.kr>
2024-05-23 19:13:44 +0900

    REFACTOR: Change Trainer to object-oriented

--- trainer.py
+++ trainer.py
@@ -6,87 +6,99 @@ from data_tokenizer import DataTokenizer
 from tqdm.auto import tqdm
 from sklearn.metrics import accuracy_score, f1_score
 
-if torch.backends.mps.is_available():
-    device = torch.device("mps")
-elif torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
 
+class TextClassifier:
+    def __init__(self, model_name, num_labels, train_data_path, device=None, learning_rate=3e-5, batch_size=4,
+                 num_epochs=5):
+        self.model_name = model_name
+        self.num_labels = num_labels
+        self.train_data_path = train_data_path
+        self.device = device if device else self.get_device()
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
 
-def train(model, data_loader, optimizer):
-    model.train()
-    loss_fn = torch.nn.CrossEntropyLoss()
-    print("Training start")
+        self.tokenizer = DataTokenizer(self.model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=self.num_labels).to(
+            self.device)
+        self.optimizer = AdamW(self.model.parameters(), lr=self.learning_rate)
+        self.train_loader, self.val_loader = self.prepare_data()
 
-    num_epochs = 5
-    num_training_steps = len(data_loader) * num_epochs
-    num_warmup_steps = int(0.1 * num_training_steps)
+    def get_device(self):
+        if torch.backends.mps.is_available():
+            return torch.device("mps")
+        elif torch.cuda.is_available():
+            return torch.device("cuda")
+        else:
+            return torch.device("cpu")
 
-    # Initialize the scheduler
-    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps,
-                                                num_training_steps=num_training_steps)
+    def prepare_data(self):
+        train_dataset, val_dataset = self.tokenizer.get_train_test_split_data(self.train_data_path)
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False)
+        return train_loader, val_loader
 
-    for epoch in range(num_epochs):
-        progress_bar = tqdm(data_loader, desc=f"Training Epoch {epoch + 1}")
-        for batch in progress_bar:
-            optimizer.zero_grad()
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-            outputs = model(input_ids, attention_mask=attention_mask)
-            loss = loss_fn(outputs.logits, labels)
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
+    def train(self):
+        self.model.train()
+        loss_fn = torch.nn.CrossEntropyLoss()
+        print("Training start")
 
-            progress_bar.set_postfix({'loss': loss.item()})
+        num_training_steps = len(self.train_loader) * self.num_epochs
+        num_warmup_steps = int(0.1 * num_training_steps)
+        scheduler = get_linear_schedule_with_warmup(self.optimizer, num_warmup_steps=num_warmup_steps,
+                                                    num_training_steps=num_training_steps)
 
-def evaluate(model, data_loader):
-    model.eval()
-    loss_fn = torch.nn.CrossEntropyLoss()
-    val_loss = 0
-    preds = []
-    true_labels = []
+        for epoch in range(self.num_epochs):
+            progress_bar = tqdm(self.train_loader, desc=f"Training Epoch {epoch + 1}")
+            for batch in progress_bar:
+                self.optimizer.zero_grad()
+                input_ids = batch['input_ids'].to(self.device)
+                attention_mask = batch['attention_mask'].to(self.device)
+                labels = batch['labels'].to(self.device)
+                outputs = self.model(input_ids, attention_mask=attention_mask)
+                loss = loss_fn(outputs.logits, labels)
+                loss.backward()
+                self.optimizer.step()
+                scheduler.step()
 
-    with torch.no_grad():
-        for batch in data_loader:
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-            outputs = model(input_ids, attention_mask=attention_mask)
-            loss = loss_fn(outputs.logits, labels)
-            val_loss += loss.item()
-            preds.extend(torch.argmax(outputs.logits, dim=1).cpu().numpy())
-            true_labels.extend(labels.cpu().numpy())
+                progress_bar.set_postfix({'loss': loss.item()})
 
-    val_loss /= len(data_loader)
-    accuracy = accuracy_score(true_labels, preds)
-    f1 = f1_score(true_labels, preds, average='weighted')
+    def evaluate(self):
+        self.model.eval()
+        loss_fn = torch.nn.CrossEntropyLoss()
+        val_loss = 0
+        preds = []
+        true_labels = []
 
-    return val_loss, accuracy, f1
+        with torch.no_grad():
+            for batch in self.val_loader:
+                input_ids = batch['input_ids'].to(self.device)
+                attention_mask = batch['attention_mask'].to(self.device)
+                labels = batch['labels'].to(self.device)
+                outputs = self.model(input_ids, attention_mask=attention_mask)
+                loss = loss_fn(outputs.logits, labels)
+                val_loss += loss.item()
+                preds.extend(torch.argmax(outputs.logits, dim=1).cpu().numpy())
+                true_labels.extend(labels.cpu().numpy())
 
-def save_model(model, path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    torch.save(model.state_dict(), path)
-    print(f"Model saved to {path}")
+        val_loss /= len(self.val_loader)
+        accuracy = accuracy_score(true_labels, preds)
+        f1 = f1_score(true_labels, preds, average='weighted')
+
+        return val_loss, accuracy, f1
+
+    def save_model(self, path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        torch.save(self.model.state_dict(), path)
+        print(f"Model saved to {path}")
+
 
 if __name__ == '__main__':
-    tokenizer = DataTokenizer("bert-base-multilingual-cased")
-    train_dataset, val_dataset = tokenizer.get_train_test_split_data('data/label_data.csv')
-
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
-
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-multilingual-cased", num_labels=4).to(device)
-
-    optimizer = AdamW(model.parameters(), lr=3e-5)
-
-    train(model, train_loader, optimizer)
-
-    val_loss, accuracy, f1 = evaluate(model, val_loader)
+    classifier = TextClassifier(model_name="bert-base-multilingual-cased", num_labels=4,
+                                train_data_path='data/label_data.csv')
+    classifier.train()
+    val_loss, accuracy, f1 = classifier.evaluate()
     print(f"Validation Loss: {val_loss}")
     print(f"Accuracy: {accuracy}")
     print(f"F1 Score: {f1}")
-
-    save_model(model, 'model/saved_model.pth')
+    classifier.save_model('model/saved_model.pth')
