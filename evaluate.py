6f877fe0147dbce4ff7a45e0ba5105fc7d851a06
hanwool95 <hanwool95@snu.ac.kr>
2024-05-24 15:42:37 +0900

    FEATURE: evaluate module

---
+++ evaluate.py
@@ -0,0 +1,17 @@
+from trainer import TextClassifier
+
+if __name__ == '__main__':
+    classifier = TextClassifier(
+        model_name="bert-base-multilingual-cased",
+        num_labels=4,
+        train_data_path='data/label_data.csv'
+    )
+
+    # Load the pre-trained model
+    classifier.load_model('model/24_05_24_43_refined_model.pth')
+
+    # Evaluate the refined model
+    val_loss, accuracy, f1 = classifier.evaluate()
+    print(f"Validation Loss: {val_loss}")
+    print(f"Accuracy: {accuracy}")
+    print(f"F1 Score: {f1}")
